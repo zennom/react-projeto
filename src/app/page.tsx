2432553c93323e7819b58bfdad6703793c05cf89
VIVIANE DE LIMA FRANCELINO <viviane.lfrancelino@senacsp.edu.br>
2024-01-16 09:17:49 -0300

    importando e exportando arquivos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+/* importe o Circle */
+import { Square , Circle} from "@/components/Square" 
 const Page = () =>{
   return(
     <div>
       <h1 className="font-bold text-lg">Olá Mundo</h1>
       <h3>Algum outro texto</h3>
+
+      
+      <Square />
+
     </div>
   )
 }
-
 export default Page
+
---
+++ src/components/Square.tsx
@@ -0,0 +1,19 @@
+export const Square = () =>{
+    return(
+        <div className="w-52 h-52 bg-orange-700 text-white">
+            Texto inspirador
+        </div>
+    )
+}
+
+export const Circle = () =>{
+    return(
+        <div className="w-52 h-52 bg-orange-700 text-white rounded-full">
+            Texto inspirador
+        </div>
+    )
+}
+
+
+
+
